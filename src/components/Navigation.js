293e44796c6b84ac3b69f76685443ad4d425064c
elantra jashari <elantrajashari@outlook.com>
2022-08-02 16:23:03 +0200

    small changes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -159,15 +159,7 @@ function Navigation() {
           <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
         </div>
       </header>
-      <main>
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          {/* <!-- Replace with your content --> */}
-          <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
-          </div>
-          {/* <!-- /End replace --> */}
-        </div>
-      </main>
+     
     </div>
   );
 }
