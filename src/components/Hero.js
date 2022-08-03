d189879b893c159548ee3f2e98b048f822613ca3
glaukaliu <glauk.aliu@gmail.com>
2022-08-03 16:09:15 +0200

    responsive fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='bg-black bg-opacity-50 relative'>
         <img src={HeroImage} className='hero-img w-full h-full object-cover absolute mix-blend-multiply' alt='hero'></img>
-        <h1 className='m:text-6xl md:text-7xl lg:text-8xl s pb-24 pt-24 m-0 text-white font-medium text-center relative'>EXPLORE BALKAN</h1>
+        <h1 className='text-5xl sm:text-6xl md:text-7xl lg:text-8xl s pb-24 pt-24 m-0 text-white font-medium text-center relative'>EXPLORE BALKAN</h1>
     </div>
   )
 }
