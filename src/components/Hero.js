6161f55495c9a121671ea4620ebbefbc9a45901a
glaukaliu <glauk.aliu@gmail.com>
2022-08-03 15:55:34 +0200

    Modified Hero Component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import './hero.css'
+import HeroImage from '../assets/img/hero-bg.jpg'
 
 const Hero = () => {
   return (
-    <div className='hero'>
-        <h1 className='hero-title'>EXPLORE BALKAN</h1>
+    <div className='bg-black bg-opacity-50 relative'>
+        <img src={HeroImage} className='hero-img w-full h-full object-cover absolute mix-blend-multiply' alt='hero'></img>
+        <h1 className='m:text-6xl md:text-7xl lg:text-8xl s pb-24 pt-24 m-0 text-white font-medium text-center relative'>EXPLORE BALKAN</h1>
     </div>
   )
 }
