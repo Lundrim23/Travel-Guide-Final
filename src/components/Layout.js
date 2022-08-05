8e35135963090f74444f3cd8fdcb546745b3d395
LorikLoku <109234586+LorikLoku@users.noreply.github.com>
2022-08-05 12:27:45 +0200

    Register Content Divider

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,14 +2,12 @@ import React from "react";
 import Footer from "../components/Footer";
 import Navigation from "../components/Navigation";
 import { Outlet } from "react-router-dom";
-import LogoSlider from "../components/LogoSlider";
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Navigation />
       <Outlet />
-      <LogoSlider />
       <Footer />
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
+import LogoSlider from "../components/LogoSlider";
+import RegisterDivider from "../components/RegisterDivider";
 
 function Home() {
   return (
     <>
       <Hero />
-      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <div className="container mx-auto px-4 flex flex-row gap-2 my-10">
-          <h1>Home</h1>
-        </div>
-      </div>
+      <LogoSlider />
+      <RegisterDivider />
     </>
   );
 }
