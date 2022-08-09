a4eb3d5c87b94676c2a7ac5f411abe0b5321bffa
elantra jashari <elantrajashari@outlook.com>
2022-08-09 14:50:26 +0200

    Created first events section

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,10 +1,13 @@
 import React from "react";
-
+import EventSection from "../components/EventSection";
 function Events() {
   return (
-    <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-      
-    </div>
+    // <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+       <div>
+  <EventSection/>
+</div>
+      //  </div>
+    
   );
 }
 
