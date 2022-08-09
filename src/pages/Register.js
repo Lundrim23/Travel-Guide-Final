723fd38be33f26116542e4d06ac8673879b4b46b
Bupka <ejup.pllashniku@gmail.com>
2022-08-09 17:23:13 +0200

    extracts user-validation.schema on new file and made a button disabled until the form is valid

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,7 +3,7 @@ import { FaRegEnvelope } from "react-icons/fa";
 import { MdLockOutline } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { useFormik } from "formik";
-import * as Yup from "yup";
+import { userSchema } from "./Utils/Schema/user-validation.schema";
 
 function Register() {
   const formik = useFormik({
@@ -14,42 +14,12 @@ function Register() {
       password: "",
       confirmPassword: "",
     },
-    validationSchema: Yup.object({
-      username: Yup.string()
-        .min(3, "Must be at least 3 characters long")
-        .max(15, "Must 15 characters or less")
-        .required("This field is Required"),
-
-      email: Yup.string()
-        .email("Invalid email")
-        .required("This field is Required"),
-
-      phone: Yup.string()
-        .required("This field is Required")
-        .matches(
-          /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
-          "Phone number is not valid"
-        )
-        .max(15),
-
-      password: Yup.string()
-        .min(8, "Password must be 8 characters long")
-        .matches(/[0-9]/, "Password requires a number")
-        .matches(/[a-z]/, "Password requires a lowercase letter")
-        .matches(/[A-Z]/, "Password requires an uppercase letter")
-        .matches(/[^\w]/, "Password requires a symbol")
-        .required("This field is Required"),
-
-      confirmPassword: Yup.string()
-        .oneOf([Yup.ref("password"), null], 'Must match "password" field value')
-        .required("This field is Required"),
-    }),
+    validationSchema: userSchema,
     onSubmit: async (values, actions) => {
-      console.log(values);
-      console.log(actions);
-      await new Promise((reset) => setTimeout(reset, 1000));
-      actions.resetForm();
-      console.log("submitted");
+      if (formik.isValid()) {
+        await new Promise((reset) => setTimeout(reset, 1000));
+        actions.resetForm();
+      }
     },
   });
   return (
@@ -219,8 +189,13 @@ function Register() {
                 </div>
 
                 <button
+                  disabled={!(formik.isValid && formik.dirty)}
                   type="submit"
-                  className="border-2 border-blue-900 text-blue-900 mb-3 rounded-full px-12 py-2 inline-block font-semibold hover:bg-blue-900 hover:text-white"
+                  className={
+                    !formik.isValid && formik.errors
+                      ? "border-2 border-blue-900 text-blue-900 mb-3 rounded-full px-12 py-2 inline-block font-semibold hover:bg-blue-900 hover:text-white"
+                      : "border-2 border-blue-900 text-blue-900 mb-3 rounded-full px-12 py-2 inline-block font-semibold "
+                  }
                 >
                   Register
                 </button>
