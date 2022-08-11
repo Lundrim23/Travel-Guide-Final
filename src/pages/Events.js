36662742287056668bb7c5da8d457822f3a68ee2
glaukaliu <glauk.aliu@gmail.com>
2022-08-11 15:44:29 +0200

    Reviews Component

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ReviewSlider from "../components/ReviewSlider";
+import { data } from "../Data/EventsReviewData";
 
 function Events() {
   return (
-    <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-      <h1>Events</h1>
-    </div>
+    <>
+      <ReviewSlider reviews={data} />
+    </>
   );
 }
 
