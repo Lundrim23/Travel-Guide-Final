4f9d7ef3165c0299ddd0f9e2f180bb8459adad68
elantra jashari <elantrajashari@outlook.com>
2022-08-11 11:35:35 +0200

    Redesigned page, added another section

--- src/components/EventTitle.js
+++ src/components/EventTitle.js
@@ -7,9 +7,9 @@ const EventTitle = () => {
     <div>
           <div>
               <div class="title m">
-                  <h2 class="text-center text-4xl font-bold text-stone-100 mb-4">Events to visit</h2>
+                  <h2 class="text-center text-4xl font-bold text-stone-50 mb-4">Events to visit</h2>
 
-                  <p class=" text-center text-2xl font-light text-stone-100">
+                  <p class=" text-center text-2xl font-light text-stone-50">
                       All article are verified by 2 experts and valdiate by the CTO
                   </p>
               </div>
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -6,13 +6,33 @@ import EventTitle from "../components/EventTitle";
 
 function Events() {
   return (
-    // <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+   
     <div>
-      <EventSection />
+    <EventHero />
+    <div class="bg-slate-50">
+      <p class="text-center text-4xl font-bold text-gray-700 pt-20 mb-4">Best Events</p>
+      <p class=" text-center text-2xl font-light text-gray-700">
+                      All article are verified by 2 experts and valdiate by the CTO
+                  </p>
+    </div>
+      <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  bg-gradient-to-b from-slate-50 to-gray-300 pt-10 ">
+      <EventCard EventPlace="Prishtine,Kosove" EventName="SunnyHill" EventText="hwhfowfcohofcowfhcow" />
+        <EventCard EventPlace="Prishtine,Kosove" EventName="SunnyHill" EventText="hwhfowfcohofcowfhcow" />
+        <EventCard EventPlace="Prishtine,Kosove" EventName="SunnyHill" EventText="hwhfowfcohofcowfhcow" />
+    </div>
 
+
+    <div class="h-40 bg-gray-300">
+      </div>
+
+
+
+      <EventSection />
       <div class="bg-gray-300 pt-20">
         <EventTitle />
       </div>
+
+    
       <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  bg-gradient-to-b from-gray-300 to-slate-50">
         <EventCard EventPlace="Prishtine,Kosove" EventName="SunnyHill" EventText="hwhfowfcohofcowfhcow" />
         <EventCard EventPlace="Prishtine,Kosove" EventName="SunnyHill" EventText="hwhfowfcohofcowfhcow" />
@@ -20,14 +40,18 @@ function Events() {
         <EventCard EventPlace="Prishtine,Kosove" EventName="SunnyHill" EventText="hwhfowfcohofcowfhcow" />
         <EventCard EventPlace="Prishtine,Kosove" EventName="SunnyHill" EventText="hwhfowfcohofcowfhcow" />
         <EventCard EventPlace="Prishtine,Kosove" EventName="SunnyHill" EventText="hwhfowfcohofcowfhcow" />
-
       </div>
       <div class="h-60 bg-slate-50">
       </div>
-      <EventHero />
+
+
+     
+
+
+
+
+
     </div>
-
-
   );
 }
 
