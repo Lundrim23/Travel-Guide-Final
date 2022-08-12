4bd5c25ebde1a5e1366086b3a4de14d1d2bc12a2
LorikLoku <109234586+LorikLoku@users.noreply.github.com>
2022-08-12 16:20:32 +0200

    Solved Homepage Errors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
 import Card from "../components/Card";
 import Hero from "../components/Hero";
 import LogoSlider from "../components/LogoSlider";
@@ -23,7 +22,7 @@ function Home() {
    
         {/*  */}
         
-        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-12">
             
           <Card countryName="Kosova" contentName="Work at home, remote, is the new age of the job, every  person can work at home...." userName="Elantra Jashari" dateName="20 mars 2029 - 6 min read"/>
           <Card countryName="Albania" contentName="Work at home, remote, is the new age of the job, every  person can work at home...." userName="Elantra Jashari" dateName="20 mars 2029 - 6 min read"/>
