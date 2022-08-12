c6b4d6c7f6e42d2e93a3e02f936256beff99bc9b
Arndrit <95188989+arndritb@users.noreply.github.com>
2022-08-12 15:25:45 +0200

    installed clodinary-react , trying to implement cloudinary for photos

--- src/components/adminComponent/EventComponent.js
+++ src/components/adminComponent/EventComponent.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import AddEvent from "../adminComponent/AddEvent";
 import axios from "axios";
+import {Image} from 'cloudinary-react'
 
 const EventComponent = () => {
   const [input, setInput] = useState({
     eventName: "",
     eventOrganizator: "",
     eventTags: "",
+    location: "",
+    address: ""
   });
 
   //this one populates state with data
@@ -17,6 +20,7 @@ const EventComponent = () => {
       return {
         ...prevInput,
         [name]: value,
+        
       };
     });
   }
@@ -29,6 +33,8 @@ const EventComponent = () => {
       eventName: input.eventName,
       eventOrganizator: input.eventOrganizator,
       eventTags: input.eventTags,
+      location: input.location,
+      address: input.address,
     };
 
     axios.post("http://localhost:5000/api/events/", newEvent);
@@ -79,6 +85,33 @@ const EventComponent = () => {
       .catch((err) => console.error(err));
   }
 
+  //radio button venue value
+  const handleVenueChange = () => {
+    setInput({
+      ...input,
+      location:"Venue"
+    })
+  }
+//radio button online value
+  const handleOnlineChange = () => {
+    setInput({
+      ...input,
+      location:"Online"
+    })
+  }
+
+  const [imageSelected, setImageSelected] = useState("")
+
+  const uploadImage = () => {
+    const formData = new FormData()
+    formData.append("file", imageSelected)
+    formData.append("upload_preset", "jipopo2x")
+
+    axios.post('https://api.cloudinary.com/v1_1/starlabstitans/image/upload', formData).then((response) =>{
+      console.log(response);
+    })
+  };
+
   return (
     <>
       {/* Popup starts here */}
@@ -105,21 +138,60 @@ const EventComponent = () => {
             value={input.eventTags}
             placeholder="Event Tags"
           ></input>
+          <input
+          onChange={e => {handleChange(e); handleVenueChange()}}
+            type="radio"
+            id="radio-example-1"
+            name="radio"
+            value={input.radio}
+            //onChange={handleVenueChange}
+          />
+          <label for="radio-example-1" class="text-gray-600">
+            Venue
+          </label>
+          <input
+          onChange={e => {handleChange(e); handleOnlineChange()}}
+            type="radio"
+            id="radio-example-2"
+            name="radio"
+            value={input.radio}
+            //onChange={handleOnlineChange}
+          />
+          <label for="radio-example-2" class="text-gray-600">
+            Online
+          </label>
+          <input
+            onChange={handleChange}
+            name="address"
+            value={input.address}
+            placeholder="Event Address"
+          ></input>
           <button onClick={handleClick}>Add Event</button>
         </form>
+        <hr />
+
+        <input 
+        type="file" 
+        onChange={(event) => {
+          setImageSelected(event.target.files[0])}}>
+        </input>
+        <button onClick={uploadImage()} >Upload Image</button>
+
+        <Image cloudName="starlabstitans" className="w-12"
+          publicId="https://res.cloudinary.com/starlabstitans/image/upload/v1660307502/mih6dncv2fspwsuqql2w.png"
+        />
 
         <table class="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
           <thead class="text-white">
             <tr class="bg-gray-300 text-gray-700 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
               <th class="p-3 text-left">Id</th>
-              <th class="p-3 text-left">Name</th>
-              <th class="p-3 text-left">Email</th>
-              <th class="p-3 text-left">Password</th>
-              <th class="p-3 text-left" width="110px">
-                Role
-              </th>
-              <th class="p-3 text-left">Delete</th>
+              <th class="p-3 text-left">Event Name</th>
+              <th class="p-3 text-left">Event Organizator</th>
+              <th class="p-3 text-left">Event Tags</th>
+              <th class="p-3 text-left">Location</th>
+              <th class="p-3 text-left">Address</th>
               <th class="p-3 text-left">Update</th>
+              <th class="p-3 text-left">Delete</th>
             </tr>
           </thead>
           <tbody class="flex-1 sm:flex-none">
@@ -138,14 +210,17 @@ const EventComponent = () => {
                   <td class="border-grey-light border hover:bg-gray-100 p-3">
                     {event.eventTags}
                   </td>
-                  <td class="border-grey-light border hover:bg-gray-100 p-3 text-yellow-800 hover:font-medium cursor-pointer">
-                    Admin
+                  <td class="border-grey-light border hover:bg-gray-100 p-3">
+                    {event.location}
                   </td>
                   <td class="border-grey-light border hover:bg-gray-100 p-3">
-                    <button onClick={() => remove(event._id)}>Delete</button>
+                    {event.address}
                   </td>
                   <td class="border-grey-light border hover:bg-gray-100 p-3">
                     <button onClick={() => update(event._id)}>Update</button>
+                  </td>
+                  <td class="border-grey-light border hover:bg-gray-100 p-3">
+                    <button onClick={() => remove(event._id)}>Delete</button>
                   </td>
                 </tr>
               </>
