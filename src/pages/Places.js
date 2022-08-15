7140a3503d21b0d9206fe5dc1c55087d0dc24bfa
elantra jashari <elantrajashari@outlook.com>
2022-08-15 16:29:16 +0200

    Added Albanian section

--- src/pages/Places.js
+++ src/pages/Places.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReviewSlider from "../components/ReviewSlider";
 import { data } from "../Data/PlacesReviewData";
+import PlacesAlbania from "../components/PlacesAlbania";
 
 function Places() {
   return (
     <>
       <ReviewSlider reviews={data} />
+      <PlacesAlbania />
     </>
   );
 }
