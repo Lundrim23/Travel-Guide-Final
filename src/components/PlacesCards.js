3bce83ce49f707d0fe0b31c62be807f994ae12a2
elantra jashari <elantrajashari@outlook.com>
2022-08-16 11:49:24 +0200

    added cards to albanian section

--- src/components/PlacesCards.js
+++ src/components/PlacesCards.js
@@ -3,27 +3,14 @@ import React from 'react'
 const PlacesCards = () => {
   return (
     <div>
-         <div className="rounded-xl relative">
-        {/* Overlay */}
-        <div className="absolute w-full h-full bg-black/5 rounded-xl text-white">
-          <p className="font-bold text-2xl px-2 pt-4">Albania</p>
-          <p className="px-2">lifhiwehfiofolfole</p>
-          <a
-            href=""
-            className="bg-transparent hover:bg-[#6366F1] text-white font-bold hover:text-white py-2 
-            px-4 border border-white mx-2 rounded absolute bottom-4 cursor-pointer"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Read more
-          </a>
-        </div>
-        <img
-          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
-          src="https://albaniamyway.com/wp-content/uploads/2020/08/ksamil-beach-saranda-albania.jpg"
-          alt=""
-        />
-      </div>
+           <div class="mx-auto flex w-96 flex-col bg-white rounded-2xl shadow-xl shadow-slate-300/60 mb-20 ">
+    <img class="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://albaniamyway.com/wp-content/uploads/2020/08/ksamil-beach-saranda-albania.jpg" />
+    <div class="p-4">
+      <small class="text-emerald-800 text-xs">Tirane, Albania</small>
+      <h1 class="text-2xl font-medium text-emerald-900 pb-2">Albania</h1>
+      <p class="text-sm tracking-tight font-light text-slate-400 leading-6">test test test</p>
+    </div>
+  </div>
     </div>
   )
 }
