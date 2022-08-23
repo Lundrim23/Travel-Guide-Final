e7aac185cb294153260cc1f01e809752813c139f
elantra jashari <elantrajashari@outlook.com>
2022-08-23 11:59:08 +0200

    small changes

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -15,19 +15,20 @@ function CommentSection() {
       <h1 class="text-center text-2xl font-bold text-gray-400 mb-10">ADD YOUR REVIEW</h1>
       <div class="space-y-4">
         <div>
+        <label for="Rating" class="block mb-2 text-lg font-bold text-gray-700">Rate your experince in this place:</label>
      <Stars />
         </div>
         <div>
           <label for="description" class="block mb-2 text-lg font-bold text-gray-700">Description:</label>
-          <textarea id="description" cols="30" rows="10" placeholder="Write..." class="w-full font-serif  p-4 text-gray-600 bg-gray-100 outline-none rounded-md"></textarea>
+          <textarea id="description" cols="30" rows="10" placeholder="Write your review..." class="w-full font-serif  p-4 text-gray-600 bg-gray-100 outline-none rounded-md"></textarea>
         </div>
         <div>
           <label for="email" class="text-lx font-bold text-gray-700">Email:</label>
-          <input type="text" placeholder="Email" id="email" class="ml-9 outline-none py-1 px-8 text-md border-2 rounded-md" />
+          <input type="text"  id="email" class="ml-9 outline-none py-1 px-4 text-md border-2 rounded-md" />
         </div>
         <div>
           <label for="email" class="text-lx font-bold text-gray-700">Password:</label>
-          <input type="text" placeholder="Password" id="password" class="ml-2 mb-4 outline-none py-1 px-2 text-md border-2 rounded-md" />
+          <input type="text"  id="password" class="ml-2 mb-4 outline-none py-1 px-2 text-md border-2 rounded-md" />
         </div>
         <button class=" px-6 py-2 mx-auto block 
         rounded-md text-lg font-semibold text-white bg-indigo-400  ">ADD REVIEW</button>
