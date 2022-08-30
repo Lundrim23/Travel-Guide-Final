ceafe216c6e6934e128875865882c0e3a4a6445f
Arndrit <95188989+arndritb@users.noreply.github.com>
2022-08-30 11:24:20 +0200

    update fetch.js

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
  const api = axios.create({
-  // baseURL: process.env.REACT_APP_BASE_URL
-  baseURL: "http://localhost:5000/api"
+  baseURL: process.env.REACT_APP_BASE_URL
 });
 
 export const cloudinaryApi = axios.create({
