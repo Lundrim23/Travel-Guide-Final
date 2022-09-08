c6b19dd7aefb8a54408ce46a3e0bbd6f981310f2
Arndrit <95188989+arndritb@users.noreply.github.com>
2022-09-08 14:55:23 +0200

    fetched events and added like/unlike

--- src/redux/features/posts/postsSlice.js
+++ src/redux/features/posts/postsSlice.js
@@ -1,13 +1,21 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-export const getPosts = createAsyncThunk(
-  "posts/getPosts",
-  async () => {
-    return fetch("http://localhost:5000/api/events/get").then((res) =>
-      res.json()
-    );
-  }
-);
+// export const getPosts = createAsyncThunk("posts/getPosts", async () => {
+//   return fetch("http://localhost:5000/api/events/get").then((res) =>
+//     res.json()
+//   );
+// });
+
+export const getPosts = createAsyncThunk("posts/getPosts", async () => {
+  const response = await axios.get("http://localhost:5000/api/events/get");
+  return response.data;
+});
+
+export const likePost = createAsyncThunk("like/likePost", async (id) => {
+  const like = await axios.put(`http://localhost:5000/api/events/like/${id}`);
+  console.log("like", like);
+});
 
 const postsSlice = createSlice({
   name: "posts",
@@ -26,7 +34,12 @@ const postsSlice = createSlice({
     [getPosts.rejected]: (state, action) => {
       state.status = "failed";
     },
-  },
+
+    [likePost.fulfilled]: (state, action) => {
+      console.log("Saying", action.payload);
+      
+    }
+    },
 });
 
 export default postsSlice.reducer;
