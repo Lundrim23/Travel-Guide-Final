be28e1712e323e90fb529bb7a12c9dd21ea34134
Bupka <ejup.pllashniku@gmail.com>
2022-09-13 14:11:30 +0200

    restaurants card displaying on map and the list

--- src/pages/Api.js
+++ src/pages/Api.js
@@ -5,11 +5,14 @@ import { getPlacesData, getPlaces } from "../api";
 import Header from "../api-components/Header/Header";
 import List from "../api-components/List/List";
 import Map from "../api-components/Map/Map";
+import userEvent from "@testing-library/user-event";
 
 const Api = () => {
   const [places, setPlaces] = useState([]);
+  const [childClicked, setChildClicked] = useState([]);
   const [coordinates, setCoordinates] = useState();
   const [bounds, setBounds] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -20,25 +23,34 @@ const Api = () => {
   }, []);
 
   useEffect(() => {
+    setIsLoading(true);
     if (bounds) {
       getPlaces(bounds.sw, bounds.ne).then((data) => {
         setPlaces(data);
+        setIsLoading(false);
       });
     }
   }, [coordinates, bounds]);
+
   return (
     <>
       <CssBaseline />
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
-          <List places={places} />
+          <List
+            places={places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+          />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map
             setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
+            places={places}
+            setChildClicked={setChildClicked}
           />
         </Grid>
       </Grid>
