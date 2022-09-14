b876c38e7fe82035e960ce61caec1c2741bc119c
Arndrit <95188989+arndritb@users.noreply.github.com>
2022-09-14 14:03:36 +0200

    update topbar with date and light/dark mode icon

--- src/components/adminComponent/pages/Topbar.js
+++ src/components/adminComponent/pages/Topbar.js
@@ -1,7 +1,16 @@
 import React from "react";
-import { NotificationIcon, Settings } from "../../AllSvgs";
+import { NotificationIcon, Settings, DarkIcon, LightIcon } from "../../AllSvgs";
+import useDarkMode from "../UseDarkMode";
 
 function Topbar() {
+
+    //this one gets the todays date
+    const current = new Date();
+    const date = `${current.getDate()}/${
+      current.getMonth() + 1
+    }/${current.getFullYear()}`;
+
+  const [colorTheme, setColorTheme] = useDarkMode();
   return (
     <div className="w-full h-12 sticky top-0">
       <div className="h-full px-5 flex items-center justify-between">
@@ -12,6 +21,23 @@ function Topbar() {
         </div>
 
         <div className="flex items-center">
+        <p className="text-gray-500 p-1 dark:text-gray-50 transition delay-500 mr-2">
+                    {date}
+                  </p>
+          <div
+            className="flex justify-between relative mr-2"
+            onClick={() => setColorTheme(colorTheme)}
+          >
+            {colorTheme === "light" ? (
+              <button className="cursor-pointer">
+                <DarkIcon />
+              </button>
+            ) : (
+              <button className="cursor-pointer">
+                <LightIcon />
+              </button>
+            )}
+          </div>
           <div className="relative cursor-pointer mr-2">
             <NotificationIcon />
             <span className="absolute -top-2 right-0 bg-red-600 text-white rounded-full w-4 h-4 flex items-center justify-center text-sm ">
