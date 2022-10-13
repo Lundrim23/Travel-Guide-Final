18dbc4d1741e49698c4be851832e4c8f70d0a48d
Arndrit <95188989+arndritb@users.noreply.github.com>
2022-10-13 16:30:54 +0200

    displayed how many users were registered in last 30 days

--- src/components/adminComponent/pages/Chart.js
+++ src/components/adminComponent/pages/Chart.js
@@ -12,36 +12,36 @@ import {
 } from "recharts";
 import { countNewUsers } from "../../../redux/features/users/userSlice";
 
-const data = [
-  {
-    name: "Page A",
-    "Active User": 4000,
-  },
-  {
-    name: "Page B",
-    "Active User": 3000,
-  },
-  {
-    name: "Page C",
-    "Active User": 2000,
-  },
-  {
-    name: "Page D",
-    "Active User": 2780,
-  },
-  {
-    name: "Page E",
-    "Active User": 1890,
-  },
-  {
-    name: "Page F",
-    "Active User": 2390,
-  },
-  {
-    name: "Page G",
-    "Active User": 3490,
-  },
-];
+// const data1 = [
+//   {
+//     name: "Page A",
+//     "Active User": 4000,
+//   },
+//   {
+//     name: "Page B",
+//     "Active User": 3000,
+//   },
+//   {
+//     name: "Page C",
+//     "Active User": 2000,
+//   },
+//   {
+//     name: "Page D",
+//     "Active User": 2780,
+//   },
+//   {
+//     name: "Page E",
+//     "Active User": 1890,
+//   },
+//   {
+//     name: "Page F",
+//     "Active User": 2390,
+//   },
+//   {
+//     name: "Page G",
+//     "Active User": 3490,
+//   },
+// ];
 
 export default function Chart() {
   const dispatch = useDispatch();
@@ -52,21 +52,43 @@ export default function Chart() {
 
   const users = useSelector((state) => state.users.allUser);
   //createdAt
-  console.log("Userat ", users);
+  //console.log("userat ", users.data);
+
+  //console.log("Userat ", users.data && users.data[0].day);
+
+  const data2 = [
+    {
+      name: users.data && users.data[0].day,
+      "Registered Users": users.data && users.data[0].count,
+    },
+    {
+      name: users.data && users.data[1].day,
+      "Registered Users": users.data && users.data[1].count,
+    },
+    {
+      name: users.data && users.data[2].day,
+      "Registered Users": users.data && users.data[2].count,
+    },
+    {
+      name: users.data && users.data[3].day,
+      "Registered Users": users.data && users.data[3].count,
+    },
+  ];
 
   return (
-      <div className="m-5 p-5 dark:bg-neutral-700 transition shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-lg">
-        {/* chart tiele */}
-        <h3 className="mb-5 dark:text-gray-300">User analytics</h3>
-        <ResponsiveContainer width="100%" aspect={4 / 1}>
-          <LineChart data={data}>
-            <XAxis dataKey="name" />
-            <Line type="monotone" dataKey="Active User" />
-            <Tooltip />
-            <CartesianGrid strokeDasharray="5 5" />
-            <Legend />
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
+    <div className="m-5 p-5 dark:bg-neutral-700 transition shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-lg">
+      <h3 className="mb-5 dark:text-gray-300">
+        Registered users in last 30 days
+      </h3>
+      <ResponsiveContainer width="100%" aspect={4 / 1}>
+        <LineChart data={data2}>
+          <XAxis dataKey="name" />
+          <Line type="monotone" dataKey="Registered Users" />
+          <Tooltip />
+          <CartesianGrid strokeDasharray="5 5" />
+          <Legend />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
